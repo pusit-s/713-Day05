6765c2c61cc5a404ae6674128e3f737bb2cf9353
Pusit.S <pusitclb@gmail.com>
2025-03-07 02:52:39 +0700

    task 7: error handling - try,catch

--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import * as service from "../services/eventService";
 import type { Event } from "../models/event";
-import exp from "constants";
+
 const router = express.Router();
 
 router.get("/", async(req, res) => {
@@ -9,13 +9,21 @@ router.get("/", async(req, res) => {
     const pageSize = parseInt(req.query.pageSize as string) || 3;
     const pageNo = parseInt(req.query.pageNo as string) || 1;
     const keyword = req.query.keyword as string;
-    const result = await service.getAllEventsWithPagination(keyword, pageSize, pageNo);
-    if (result.events.length === 0) {
+    try {
+        const result = await service.getAllEventsWithPagination(keyword, pageSize, pageNo);
+        if (result.events.length === 0) {
         res.status(404).send("No events found");
         return;
+        }
+        res.setHeader("X-Total-Count", result.count.toString());
+        res.json(result.events);
+    } catch (error) {
+        if (pageNo < 1 || pageSize < 1) {
+        res.status(400).send("Invalid page number or page size");
+        } else {
+        res.status(500).send("Internal Server Error");}
     }
-    res.setHeader("X-Total-Count", result.count.toString());
-    res.json(result.events);
+    return;
     } else if (req.query.category) {
     const category = req.query.category;
     const filteredEvents = await service.getEventByCategory(category as string);
