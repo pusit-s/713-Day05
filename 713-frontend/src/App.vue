fb902ef92654b545fed615d1a4c4580dbcc8e8d6
Pusit.S <pusitclb@gmail.com>
2025-03-08 14:17:45 +0700

    task 5: component level

--- 713-frontend/src/App.vue
+++ 713-frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Event</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
         <RouterView />
--- 713-frontend/src/views/EventLisView.vue
+++ 713-frontend/src/views/EventLisView.vue
@@ -48,6 +48,7 @@ const events = ref<Event[]>([
 
 
 <template>
+  <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
