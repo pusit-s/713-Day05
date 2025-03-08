d39cbf5b6b35f2f62dd90ac1f104128b06d4ad3b
Pusit.S <pusitclb@gmail.com>
2025-03-08 10:24:33 +0700

    task 2: create component

--- 713-frontend/src/views/EventLisView.vue
+++ 713-frontend/src/views/EventLisView.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+
+</script>
+
 
 <template>
   <div></div>
+  <div>
+    <EventCard />
+    <EventCard />
+    <EventCard />
+  </div>
 </template>
