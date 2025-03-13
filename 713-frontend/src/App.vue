18bbdec742f7e4b253dab8e51ba2c06acdfc8fa9
Pusit.S <pusitclb@gmail.com>
2025-03-14 02:58:44 +0700

    task 13: pinia

--- 713-frontend/src/App.vue
+++ 713-frontend/src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from './stores/message';
+import { storeToRefs } from 'pinia';
+const store = useMessageStore()
+const { message } = storeToRefs(store)
 </script>
 
 <template>
   <div id="layout">
     <header>
+      <div id="flash-message" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
       <div class="wrapper">
         <nav>
           <RouterLink :to="{ name: 'event-list-view' }">Events</RouterLink>
@@ -37,5 +44,17 @@ nav a.router-link-exact-active {
 
 h2 {
   font-size: 20px;
+}
+
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
 }
 </style>
