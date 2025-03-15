a06364da5429215ebef43367ac674b05f9e1b63e
Pusit.S <pusitclb@gmail.com>
2025-03-15 12:41:34 +0700

    deployment

--- 713-backend/src/index.ts
+++ 713-backend/src/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 import { uploadFile } from './services/uploadFileService';
 const app = express();
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://713-2024-frontend.vercel.app'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
